8400ab8708b4b82d7fad82613b8091c26012c108
YM <yangming@1yd.me>
2016-03-03 11:29:31 +0800

    init

---
+++ YMApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YMApp
+//
+//  Created by YM on 16/3/3.
+//  Copyright © 2016年 YM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
